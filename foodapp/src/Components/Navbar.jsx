a73fa6638d7d442c86d1e187fd2f286fe700c1ab
Siddhanta Shrestha <sidmarkys2004@gmail.com>
2024-02-22 17:00:23 +0545

    second push

--- foodapp/src/Components/Navbar.jsx
+++ foodapp/src/Components/Navbar.jsx
@@ -25,75 +25,82 @@ export default function Navbar() {
   const items = useCart();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
-      <div className="container">
-        <Link className="navbar-brand fs-1 fst-italic" to="/">
-          FoodWeb
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link fs-5 mx-3" aria-current="page" to="/">
-                Home
-              </Link>
-            </li>
-            {localStorage.getItem("token") && (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success position-sticky">
+        <div className="container">
+          <Link className="navbar-brand fs-1 fst-italic" to="/">
+            FoodWeb
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link fs-5 mx-3" to="/myorder">
-                  My Orders
+                <Link className="nav-link fs-5 mx-3" aria-current="page" to="/">
+                  Home
                 </Link>
               </li>
-            )}
-          </ul>
-          {!localStorage.getItem("token") ? (
-            <form className="d-flex">
-              <Link className="btn btn-light text-success mx-1" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-light text-success mx-1" to="/signup">
-                Signup
-              </Link>
-            </form>
-          ) : (
-            <div className="d-flex align-items-center">
-              <div
-                className="btn bg-white text-success mx-2"
-                onClick={loadCart}
-              >
-                <Badge color="secondary" badgeContent={items.length}>
-                  <ShoppingCartIcon />
-                </Badge>
-                {"  "}
-                Cart
-              </div>
-
-              {cartView && (
-                <Modal onClose={() => setCartView(false)}>
-                  <Cart />
-                </Modal>
+              {localStorage.getItem("token") && (
+                <li className="nav-item">
+                  <Link className="nav-link fs-5 mx-3" to="/myorder">
+                    My Orders
+                  </Link>
+                </li>
               )}
+            </ul>
+            {!localStorage.getItem("token") ? (
+              <form className="d-flex">
+                <Link className="btn btn-light text-success mx-1" to="/login">
+                  Login
+                </Link>
+                <Link
+                  className="btn btn-light text-success mx-1"
+                  to="/registerUser"
+                >
+                  Signup
+                </Link>
+              </form>
+            ) : (
+              <div className="d-flex align-items-center">
+                <div
+                  className="btn bg-white text-success mx-2"
+                  onClick={loadCart}
+                >
+                  <Badge color="secondary" badgeContent={items.length}>
+                    <ShoppingCartIcon />
+                  </Badge>
+                  {"  "}
+                  Cart
+                </div>
 
-              <button
-                onClick={handleLogout}
-                className="btn bg-white text-success"
-              >
-                Logout
-              </button>
-            </div>
-          )}
+                {cartView ? (
+                  <Modal onClose={() => setCartView(false)}>
+                    <Cart></Cart>
+                  </Modal>
+                ) : (
+                  ""
+                )}
+
+                <button
+                  onClick={handleLogout}
+                  className="btn bg-white text-success"
+                >
+                  Logout
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
